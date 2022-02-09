9f634619e44dd73948667b05a6b986d8a43ab2e2
Giovani Bontempo <injuriae@gmail.com>
2022-02-09 11:33:22 -0300

    segundo commit

---
+++ Teste4/Classes/Calculadora.cs
@@ -0,0 +1,22 @@
+namespace Teste4.Classes
+{
+    public class Calculadora
+    {
+        public double Soma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtracao(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiplicacao(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public double Divisao(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Teste4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Teste4.Classes;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora operacoes = new Calculadora();
+            Console.WriteLine(operacoes.Soma(20, 10));
+            Console.WriteLine(operacoes.Subtracao(20, 10));
+            Console.WriteLine(operacoes.Divisao(20, 10));
+            Console.WriteLine(operacoes.Multiplicacao(20, 10));
+
+            Console.WriteLine(operacoes.Soma(198, 99));
+            Console.WriteLine(operacoes.Subtracao(198, 99));
+            Console.WriteLine(operacoes.Divisao(198, 99));
+            Console.WriteLine(operacoes.Multiplicacao(198, 99));
+
+            Console.WriteLine(operacoes.Soma(11549, 9284));
+            Console.WriteLine(operacoes.Subtracao(11549, 9284));
+            Console.WriteLine(operacoes.Divisao(11549, 9284));
+            Console.WriteLine(operacoes.Multiplicacao(11549, 9284));
+
+            Console.WriteLine(operacoes.Soma(99999, 9));
+            Console.WriteLine(operacoes.Subtracao(99999, 9));
+            Console.WriteLine(operacoes.Divisao(99999, 9));
+            Console.WriteLine(operacoes.Multiplicacao(99999, 9));
+            
+        }
+    }
+}
